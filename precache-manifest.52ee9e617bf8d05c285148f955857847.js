894a1832a3f6077b53b090461202ab1d40416728
sravan <shashilsravan.ss.ss@gmail.com>
2020-08-14 13:27:27 +0530

    Updates

---
+++ precache-manifest.52ee9e617bf8d05c285148f955857847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b2c0c7f437a7d5642ceebdd7fb7c7d",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "6bc4444015b373cc19e5",
+    "url": "/ExpenseTracker/static/css/main.96a7a4c2.chunk.css"
+  },
+  {
+    "revision": "d7601c94ca093172e2fb",
+    "url": "/ExpenseTracker/static/js/2.ee78d2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.ee78d2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc4444015b373cc19e5",
+    "url": "/ExpenseTracker/static/js/main.e0949248.chunk.js"
+  },
+  {
+    "revision": "fbfbdfec0faa1d254913",
+    "url": "/ExpenseTracker/static/js/runtime-main.73d1c789.js"
+  }
+]);
